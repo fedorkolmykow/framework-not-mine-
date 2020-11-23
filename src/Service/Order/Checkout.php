b6b346b6df1e36cb72387acd5e37d646472098fb
fedor <fedorkolmykow@gmail.com>
2020-11-23 19:22:19 +0300

    add class Checkout

---
+++ src/Service/Order/Checkout.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace Service\Order;
+
+
+use Model\Entity\Product;
+use Service\Billing\Card;
+use Service\Communication\Email;
+use Service\Discount\BestDiscount;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class Checkout
+{
+
+    /**
+     * Оформление заказа
+     * @param SessionInterface $session
+     * @param Product[] $products
+     *
+     * @return void
+     */
+    public function checkout(SessionInterface $session, array $products): void
+    {
+        // Здесь должна быть некоторая логика выбора способа платежа
+        $basketBuilder = new BasketBuilder();
+        $basketBuilder->setBilling(new Card());
+        $basketBuilder->setSession($session);
+        $basketBuilder->setCommunication(new Email());
+        $basketBuilder->setDiscount(
+            (new BestDiscount())->getBestDiscount($session, $products)
+        );
+        $basketBuilder->setProducts($products);
+
+        $check = new CheckoutProcess();
+        $check->Process($basketBuilder);
+    }
+
+}
