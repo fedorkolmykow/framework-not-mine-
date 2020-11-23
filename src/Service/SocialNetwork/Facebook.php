5e05a9e037ae25ec7fd83bfa2dc545a95a46774a
fedor <fedorkolmykow@gmail.com>
2020-11-23 19:27:02 +0300

    add empty classes VK and Facebook

---
+++ src/Service/SocialNetwork/Facebook.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Service\SocialNetwork;
+
+
+class Facebook implements ISocialNetwork
+{
+    /**
+     * Отправка сообщения в соц.сеть
+     *
+     * @param string $message
+     *
+     * @return void
+     */
+    public function send(string $message): void{
+
+    }
+
+}
